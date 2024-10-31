9d855eae83a1a0b65f182de9dddc598063762232
dimtakos <dimitako@ihu.gr>
2024-10-31 20:31:18 +0200

    Create models for destinations ver.1

--- Diplomatiki/settings.py
+++ Diplomatiki/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "destinations.apps.DestinationsConfig"
+    'destinations',
 ]
 
 MIDDLEWARE = [
